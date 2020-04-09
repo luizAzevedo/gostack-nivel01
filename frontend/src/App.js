e759c550cb905a70bdbe91c6a3aee009419f65e1
Luiz Azevedo <lfaazevedo@hotmail.com>
2020-04-09 20:16:13 -0300

    Alteração do app e header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,18 @@ import React from 'react';
 import Header from './components/Header';
 
 function App() {
-  return (
+  const projects = ['Desenvolvimento de app', 'Front-end web'];
 
+  return (
     //fragment impedir a criação de um elemento, porque o react não permite dois componentes react sozinhos
-    
-    <>  
-      <Header />
-      <Header />
+    <>
+      <Header tittle="Projects">
+        <ul>
+          {projects.map(project => (
+            <li>{project}</li>
+          ))}
+        </ul>
+      </Header>
     </>
   );
 }
